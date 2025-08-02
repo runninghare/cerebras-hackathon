01b2fed191abb99b41addcf76b5d6e665e3e83ec
Ross Zhu <ross.yuexin.zhu@gmail.com>
2025-08-03 09:08:58 +1000

    Configure Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='rotate-45'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
